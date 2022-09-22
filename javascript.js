e6f92cbbcc9cd2f5413b924549d02742622035b1
JosediO <josediuepb@gmail.com>
2022-09-22 18:06:19 -0300

    fixed functions

--- javascript.js
+++ javascript.js
@@ -1,21 +1,28 @@
 let Lastsorted;
-let Table = [null,null,null];
+let Table = [null,null,null,null];
 function Play(){
     Sorted();
     
-    setInterval(Sorted,2000);
+    
 }
 
 function  Sorted(){
     Lastsorted = Math.floor(Math.random() * 10 + 1)
     console.log(Lastsorted);
+       if(!Table.includes(Lastsorted)){
         Table.unshift(Lastsorted);
-    Show();
-}
-
+        document.getElementById(Lastsorted).checked = true;
+        setTimeout(Sorted,5000);
+        Show();
+    }else{
+        console.log("Repetiu");
+        Sorted();
+    } 
+} 
 
 function Show(){
-    document.getElementById("slot1").value = Table[0];
-    document.getElementById("slot2").value = Table[1];
-    document.getElementById("slot3").value = Table[2]; 
+    document.getElementById("slot0").value = Table[0];
+    document.getElementById("slot1").value = Table[1];
+    document.getElementById("slot2").value = Table[2];
+    document.getElementById("slot3").value = Table[3]; 
 }
