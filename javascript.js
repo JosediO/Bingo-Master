3dc3e0618055c8001c45ac60190d4d4a145e273d
JosediO <josediuepb@gmail.com>
2022-09-27 15:13:51 -0300

    fixed function to sort and show numbers

--- javascript.js
+++ javascript.js
@@ -1,28 +1,31 @@
-let Lastsorted;
-let Table = [null,null,null,null];
-function Play(){
-    Sorted();
-    
-    
+let lastsorted;
+let table = [null,null,null,null];
+let numbers=[];
+let maxNumber = 85;
+function play(){
+    createNumber();
+    show();
 }
 
-function  Sorted(){
-    Lastsorted = Math.floor(Math.random() * 10 + 1)
-    console.log(Lastsorted);
-       if(!Table.includes(Lastsorted)){
-        Table.unshift(Lastsorted);
-        document.getElementById(Lastsorted).checked = true;
-        setTimeout(Sorted,5000);
-        Show();
-    }else{
-        console.log("Repetiu");
-        Sorted();
-    } 
-} 
+function createNumber(){
+    if(numbers.length == maxNumber){
+        if (confirm('Já não há mais! Quer recomeçar?')) numbers = [];
+        else return;
+    }
+    let globe = Math.ceil(Math.random()*maxNumber);
+    while(numbers.indexOf(globe)>=0){
+        globe = Math.ceil(Math.random() * maxNumber);
+    }
+    numbers.push(globe)
+    document.getElementById(globe).checked = true;
+    table.unshift(globe);
+    setTimeout(createNumber,5000);
+    show();
+}
 
-function Show(){
-    document.getElementById("slot0").value = Table[0];
-    document.getElementById("slot1").value = Table[1];
-    document.getElementById("slot2").value = Table[2];
-    document.getElementById("slot3").value = Table[3]; 
+function show(){
+    document.getElementById("slot0").value = table[0];
+    document.getElementById("slot1").value = table[1];
+    document.getElementById("slot2").value = table[2];
+    document.getElementById("slot3").value = table[3]; 
 }
