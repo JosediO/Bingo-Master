00eccc2f58902330d5d0a7b5781131987a8e284a
JosediO <josediuepb@gmail.com>
2022-10-04 09:51:54 -0300

    Fixed slot style

--- javascript.js
+++ javascript.js
@@ -1,12 +1,11 @@
 let table = [null,null,null,null];
 let numbers=[];
 let maxNumber = 75;
-let flag 
+let flag;
 function play(){
     flag = true;
     createNumber();
     show();
-    console.log("GO!")
     document.getElementById("play").disabled = true;
     document.getElementById("reload").disabled = false;
     document.getElementById("pause").disabled = false;
@@ -15,7 +14,6 @@ function pause(){
     document.getElementById("play").disabled = false;
     document.getElementById("pause").disabled = true;
     if(flag == true){
-        console.log("Parou");
         return flag = false;
     }   
 }
@@ -43,5 +41,17 @@ function show(){
     document.getElementById("slot0").value = table[0];
     document.getElementById("slot1").value = table[1];
     document.getElementById("slot2").value = table[2];
-    document.getElementById("slot3").value = table[3]; 
-}
+    document.getElementById("slot3").value = table[3];
+    if(document.getElementById("slot0") != null ){
+        document.getElementById("firstSlot").style.background = "radial-gradient(ellipse at 74% 71%, rgba(147, 147, 1, 1) 0%, rgba(247, 255, 92, 1) 95%, rgba(253, 240, 123, 1) 100%)";
+    }
+    if(table[1] != null){
+        document.getElementById("s1").style.background = "radial-gradient(ellipse at 74% 71%, rgba(147, 147, 1, 1) 0%, rgba(247, 255, 92, 1) 95%, rgba(253, 240, 123, 1) 100%)";
+    }
+    if(table[2] != null){
+        document.getElementById("s2").style.background = "radial-gradient(ellipse at 74% 71%, rgba(147, 147, 1, 1) 0%, rgba(247, 255, 92, 1) 95%, rgba(253, 240, 123, 1) 100%)";
+    }
+    if(table[3] != null){
+        document.getElementById("s3").style.background = "radial-gradient(ellipse at 74% 71%, rgba(147, 147, 1, 1) 0%, rgba(247, 255, 92, 1) 95%, rgba(253, 240, 123, 1) 100%)";
+    }
+    } //background-image: linear-gradient(to left top, #777a39, #818441, #8a8d49, #949752, #9ea15a, #a7aa66, #b1b472, #babd7e, #c6c890, #d1d4a2, #dddfb4, #e9ebc7);
