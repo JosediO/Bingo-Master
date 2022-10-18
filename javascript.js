d3ed53f4614383acecbf0faf4bde401e17c00fb9
JosediO <josediuepb@gmail.com>
2022-10-17 21:06:18 -0300

    bug fix

--- javascript.js
+++ javascript.js
@@ -1,54 +1,48 @@
 let table = [null,null,null,null];
-let numbers=[];
-let maxNumber = 75;
-let flag;
-let clock;
+let numbers=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75];
+let sorted;
+let action = 'HOME';
 
-function play(){
-    flag = true;
-    if(numbers.length > 0){
-    clock=setTimeout(createNumber,3000)
+game();
+
+function createNumber(){
+    validation();
+    let globe = Math.floor(Math.random()*numbers.length);
+    sorted = numbers[globe];
+    if(sorted != undefined){
+    numbers.splice(globe,1);
+    table.unshift(sorted);
+    document.getElementById(sorted).disabled = false;
     show();
-    document.getElementById("play").disabled = true;
-    document.getElementById("reload").disabled = false;
-    document.getElementById("pause").disabled = false;
-    }else{
-        createNumber();
-        show();
-        document.getElementById("play").disabled = true;
-        document.getElementById("reload").disabled = false;
-        document.getElementById("pause").disabled = false;
     }
+    
+}
+function play(){
+    action = 'START';
 }
 function pause(){
-    document.getElementById("pause").disabled = true;
-    document.getElementById("play").disabled = false;
-    clearTimeout(clock);
-    if(flag == true){
-        return flag = false;
-    }   
+    action = 'PAUSE';
 }
-function createNumber(){
-        if(flag == true){
-            if(numbers.length == maxNumber){
-                if (confirm('Play again?')){
-                    numbers = [];
-                    location.reload(play);  
+function restart(){
+    action = 'RESTART';
+}
+function endGame(){
+    action = 'ENDGAME';
+    document.getElementById("pause").disabled = true;
+    document.getElementById("play").disabled = true;
+
+}
+
+function validation(){
+    if(numbers.length == 0){
+        if (confirm('Play Again?') == true){
+                    location.reload(game);
+                }else{
+                    endGame();
+                    console.log(action);
+                    
                 } 
-                else return;
-            }
-            let globe = Math.ceil(Math.random()*maxNumber);
-            while(numbers.indexOf(globe)>=0){
-                globe = Math.ceil(Math.random() * maxNumber);
-            }
-            numbers.push(globe)
-            document.getElementById(globe).disabled = false;
-            table.unshift(globe);
-            clock = setTimeout(createNumber,5000);
-            show();
-        }else if(flag == false){
-            
-        }    
+    }
 }
 function show(){
     document.getElementById("slot0").value = table[0];
@@ -68,3 +62,40 @@ function show(){
         document.getElementById("s3").style.background = "radial-gradient(ellipse at 74% 71%, rgba(147, 147, 1, 1) 0%, rgba(247, 255, 92, 1) 95%, rgba(253, 240, 123, 1) 100%)";
     }
     }
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function game(){
+    while(action != 'RESTART'){
+        switch(action){
+            case 'START':
+                createNumber();
+                show();
+                document.getElementById("play").disabled = true;
+                document.getElementById("reload").disabled = false;
+                document.getElementById("pause").disabled = false;
+                await sleep(5000);
+                break;
+            
+            case 'PAUSE':
+                document.getElementById("pause").disabled = true;
+                document.getElementById("play").disabled = false; 
+                await sleep(1000);
+                break;
+            
+            case 'HOME':
+                await sleep(500);
+                break;
+            case 'ENDGAME':
+                document.getElementById("pause").disabled = true;
+                document.getElementById("play").disabled = true;
+                await sleep(500);
+                break;
+        }
+        await sleep(1000);
+        
+    }
+    await sleep(1000);       
+}
